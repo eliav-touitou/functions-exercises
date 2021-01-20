199fa8d8dc2bf1a3b45952802ffcd88e113fc200
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-01-20 18:34:07 +0200

    q5

--- Exercises.js
+++ Exercises.js
@@ -1,30 +1,40 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
 }
 
 //Question 2
 function allCombinations(str) {
   let combinations = [];
-  // your code here
-
-  //
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      combinations.push(str.slice(i, j));
+    }
+  }
   return combinations;
 }
 
 //Question 3
 function allCaps(str) {
-  // your code here
-  return "";
+  let arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+  }
+  return arr.join(" ");
 }
 
 //Question 4
 function myPower(x, n) {
-  // your code here
-  return "";
+  let pow = 1;
+  for (let i = 1; i <= n; i++) {
+    pow *= x;
+  }
+  return pow;
 }
-
 //Question 5
 function getFirstNotRepeating(str) {
   // your code here
